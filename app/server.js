39dccd97beba30647fe004218afa88fcd0fe011b
星野栄治 <177436503+HosinoEJ@users.noreply.github.com>
2026-03-16 02:47:27 +0800

    0.3

--- app/server.js
+++ app/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const axios = require('axios');
-const fetch = require('node-fetch')
 require('dotenv').config();
 const app = express();
 
